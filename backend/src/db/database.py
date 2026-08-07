6c8fabb55eab0862a621ba8596b999b7ca1c40d7
Surya_1112 <suryasamwork@gmail.com>
2026-08-07 09:17:35 +0530

    .

--- backend/src/db/database.py
+++ backend/src/db/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+pg8000://postgres:password@localhost:5432/worker_monitor")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+pg8000://postgres:password@localhost:5433/worker_monitor")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
